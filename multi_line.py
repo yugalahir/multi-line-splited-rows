f42d1556c6ef444c622e44234fc1471f67f53202
yugal ahir <yugal.ahir@gmail.com>
2021-04-24 15:41:09 +0530

    Update multi_line.py

--- multi_line.py
+++ multi_line.py
@@ -1,5 +1,7 @@
-# Developed by Yugal Kumar Ahir
-
+'''
+Developed by Yugal Kumar Ahir
+Purpose - Merging the multiple line description of the product into its parent row
+'''
 from tabula import wrapper
 import numpy as np
 import json
@@ -10,9 +12,12 @@ import yaml
 import traceback
 import pdfplumber
 import pandas as pd
-
-#line_items this is your DataFrame
-#multi_line_items rows of data frame on which you have merge - like Serial number, Index Number , unique identifer
+'''
+Function name multi_line_ext(line_items, multi_line_items)
+Parameters
+1 line_items this holds the main dataframe 
+2 multi_line_items has sub dataframe column on which we have to merge the rows e.g Serial number, Index Number , unique identifer
+'''
 def multi_line_ext(line_items, multi_line_items):
     try:
         line_items = line_items.stack().apply(pd.to_numeric, errors='ignore').fillna(0).unstack()
@@ -20,22 +25,23 @@ def multi_line_ext(line_items, multi_line_items):
         df_merge = pd.DataFrame(sd, columns=['check'])
         df_merge = df_merge[df_merge['check'] == True]
         df_merge = df_merge.sort_index(ascending=False)
-        print(df_merge, 'df_merged')
 
         for mi in multi_line_items:
-            for vv in df_merge.index:
-                if line_items.iloc[vv - 1][mi] != 0:
-                    a = line_items.iloc[vv - 1][mi]
-                    g = line_items.iloc[vv - 2][mi]
-                    l = str(g) + ' ' + str(a)
-                    l = l.replace('"', '')
-                    line_items = line_items.replace(line_items.iloc[vv - 2][mi], l)
+            for mi_sub_df in df_merge.index:
+                if line_items.iloc[mi_sub_df - 1][mi] != 0:
+                    first_line = line_items.iloc[mi_sub_df - 1][mi]
+                    next_line = line_items.iloc[mi_sub_df - 2][mi]
+                    temp_holder = str(next_line) + ' ' + str(first_line)
+                    temp_holder = temp_holder.replace('"', '')
+                    line_items = line_items.replace(line_items.iloc[mi_sub_df - 2][mi], temp_holder)
 
-        for vv in df_merge.index:
-            line_items = line_items.drop(vv)
+        for temp_cleaner in df_merge.index:
+            line_items = line_items.drop(temp_cleaner)
         line_items = line_items.replace('\n', '', regex=True)
         line_items = line_items.replace('\|', '', regex=True)
         return line_items
 
     except Exception as e:
+        #logging of errors
         return traceback.print_exc()
+#End of code
