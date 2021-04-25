1a2463e808dd830b79e622b4c8eeaf48f883759d
yugal ahir <yugal.ahir@gmail.com>
2021-04-25 13:16:14 +0530

    Update multi_line.py

--- multi_line.py
+++ multi_line.py
@@ -15,30 +15,32 @@ Parameters
 1 line_items this holds the main dataframe 
 2 multi_line_items has sub dataframe column on which we have to merge the rows e.g Serial number, Index Number , unique identifer
 '''
-def multi_line_ext(line_items, multi_line_items):
-    try:
-        line_items = line_items.stack().apply(pd.to_numeric, errors='ignore').fillna(0).unstack()
-        sd = (line_items == 0).sum(axis=1) > len(line_items.columns) / 2
-        df_merge = pd.DataFrame(sd, columns=['check'])
-        df_merge = df_merge[df_merge['check'] == True]
-        df_merge = df_merge.sort_index(ascending=False)
+class Docworx():
+        line_items = [] #initial value of line_items list to empty
+        def multi_line_ext(line_items, multi_line_items):
+            try:
+                line_items = line_items.stack().apply(pd.to_numeric, errors='ignore').fillna(0).unstack()  #cleaning the dataframe
+                sd = (line_items == 0).sum(axis=1) > len(line_items.columns) / 2    #getting the average of (sum of empty data of row divied by total number of columns)
+                df_merge = pd.DataFrame(sd, columns=['check']) 
+                df_merge = df_merge[df_merge['check'] == True]
+                df_merge = df_merge.sort_index(ascending=False)
+                #Running loop on the columns 
+                for mi in multi_line_items: 
+                    for mi_sub_df in df_merge.index:
+                        if line_items.iloc[mi_sub_df - 1][mi] != 0:
+                            first_line = line_items.iloc[mi_sub_df - 1][mi] #getting first parent rows
+                            next_line = line_items.iloc[mi_sub_df - 2][mi] #getting child rows
+                            temp_holder = str(next_line) + ' ' + str(first_line)
+                            temp_holder = temp_holder.replace('"', '') #removing "
+                            line_items = line_items.replace(line_items.iloc[mi_sub_df - 2][mi], temp_holder)
 
-        for mi in multi_line_items:
-            for mi_sub_df in df_merge.index:
-                if line_items.iloc[mi_sub_df - 1][mi] != 0:
-                    first_line = line_items.iloc[mi_sub_df - 1][mi]
-                    next_line = line_items.iloc[mi_sub_df - 2][mi]
-                    temp_holder = str(next_line) + ' ' + str(first_line)
-                    temp_holder = temp_holder.replace('"', '')
-                    line_items = line_items.replace(line_items.iloc[mi_sub_df - 2][mi], temp_holder)
+                for temp_cleaner in df_merge.index:
+                    line_items = line_items.drop(temp_cleaner)  #removing the irrelevant data
+                line_items = line_items.replace('\n', '', regex=True) #removing spaces
+                line_items = line_items.replace('\|', '', regex=True) #removing pine line
+                return line_items
 
-        for temp_cleaner in df_merge.index:
-            line_items = line_items.drop(temp_cleaner)
-        line_items = line_items.replace('\n', '', regex=True)
-        line_items = line_items.replace('\|', '', regex=True)
-        return line_items
-
-    except Exception as e:
-        #logging of errors
-        return traceback.print_exc()
-#End of code
+            except Exception as e:
+                #logging of errors
+                return traceback.print_exc()
+#End of class
